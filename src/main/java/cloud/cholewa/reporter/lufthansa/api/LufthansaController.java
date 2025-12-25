6e9e80d85433ddb2bc7e77620375294dbb4cca45
Krzysztof Cholewa <krzysztof.cholewa@gmail.com>
2025-12-25 12:50:01 +0100

    Refactor LufthansaController to integrate LufthansaService and update tests accordingly

--- src/main/java/cloud/cholewa/reporter/lufthansa/api/LufthansaController.java
+++ src/main/java/cloud/cholewa/reporter/lufthansa/api/LufthansaController.java
@@ -1,11 +1,11 @@
 package cloud.cholewa.reporter.lufthansa.api;
 
+import cloud.cholewa.reporter.lufthansa.service.LufthansaService;
 import cloud.cholewa.reporter.lufthansa.model.CreateTaskRequest;
 import cloud.cholewa.reporter.lufthansa.model.CreatedTaskResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -19,11 +19,12 @@ import reactor.core.publisher.Mono;
 @RequestMapping("/lufthansa")
 public class LufthansaController {
 
+    private final LufthansaService lufthansaService;
+
     @PostMapping("/tasks")
-    public Mono<ResponseEntity<CreatedTaskResponse>> createTask(@Valid @RequestBody CreateTaskRequest request) {
-        return Mono.just(ResponseEntity.status(HttpStatus.NOT_IMPLEMENTED).body(CreatedTaskResponse.builder()
-                .description("Not implemented yet")
-                .build()))
+    public Mono<ResponseEntity<CreatedTaskResponse>> registerTask(@Valid @RequestBody CreateTaskRequest request) {
+        return lufthansaService.registerTask(request)
+            .map(ResponseEntity::ok)
             .doOnSubscribe(subscription -> log.info("Creating task for Lufthansa"));
     }
 }
--- src/test/java/cloud/cholewa/reporter/lufthansa/api/LufthansaControllerTest.java
+++ src/test/java/cloud/cholewa/reporter/lufthansa/api/LufthansaControllerTest.java
@@ -2,18 +2,26 @@ package cloud.cholewa.reporter.lufthansa.api;
 
 import cloud.cholewa.reporter.lufthansa.model.CreateTaskRequest;
 import cloud.cholewa.reporter.lufthansa.model.CreatedTaskResponse;
+import cloud.cholewa.reporter.lufthansa.service.LufthansaService;
 import org.junit.jupiter.api.Test;
+import org.mockito.Answers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.webflux.test.autoconfigure.WebFluxTest;
-import org.springframework.http.HttpStatus;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.reactive.server.WebTestClient;
 import org.springframework.web.reactive.function.BodyInserters;
+import reactor.core.publisher.Mono;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
 
 @WebFluxTest(LufthansaController.class)
 class LufthansaControllerTest {
 
     @Autowired
     private WebTestClient webTestClient;
+    @MockitoBean(answers = Answers.RETURNS_SMART_NULLS)
+    private LufthansaService lufthansaService;
 
     @Test
     void shouldReturnBadRequestWhenDescriptionIsShorterThan10Chars() {
@@ -24,14 +32,16 @@ class LufthansaControllerTest {
     }
 
     @Test
-    void shouldReturnNotImplementedWhenDescriptionIsMin10CharsLength() {
+    void shouldReturnOKWhenDescriptionIsMin10CharsLength() {
+        when(lufthansaService.registerTask(any())).thenReturn(Mono.just(CreatedTaskResponse.builder().build()));
+
         webTestClient.post()
             .uri("/lufthansa/tasks")
             .body(BodyInserters.fromValue(
                 CreateTaskRequest.builder().description("Test description").build()
             ))
             .exchange()
-            .expectStatus().isEqualTo(HttpStatus.NOT_IMPLEMENTED)
+            .expectStatus().isOk()
             .expectBody(CreatedTaskResponse.class);
     }
 }
